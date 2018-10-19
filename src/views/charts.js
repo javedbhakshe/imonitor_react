8f73bf93b9635ff3f4adcf0fff5dbc893f1984ab
JavedAtDure <javed@duretechnologies.com>
2018-10-19 10:02:37 +0530

    Radar
    Radar

--- src/views/charts.js
+++ src/views/charts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Bar, Doughnut, Line, Pie, Polar, Radar } from 'react-chartjs-2';
+import { Bar, Doughnut, Line, Pie, /*Polar, Radar*/ } from 'react-chartjs-2';
 import { Card, CardBody, CardColumns, CardHeader } from 'reactstrap';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 
@@ -67,7 +67,7 @@ const doughnut = {
     }],
 };
 
-const radar = {
+/*const radar = {
   labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
   datasets: [
     {
@@ -91,7 +91,7 @@ const radar = {
       data: [28, 48, 40, 19, 96, 27, 100],
     },
   ],
-};
+};*/
 
 const pie = {
   labels: [
@@ -115,7 +115,7 @@ const pie = {
     }],
 };
 
-const polar = {
+/*const polar = {
   datasets: [
     {
       data: [
@@ -141,7 +141,7 @@ const polar = {
     'Grey',
     'Blue',
   ],
-};
+};*/
 
 const options = {
   tooltips: {
